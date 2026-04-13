a7da5cc2df3b97dd726cf9f26951c5a521921ada
e.marichev <e.marichev@msocialproduction.com>
2026-04-13 22:41:06 +0300

    addButton

--- frontend/src/shared/ui/buttons/addButton/addButton.tsx
+++ frontend/src/shared/ui/buttons/addButton/addButton.tsx
@@ -1,9 +1,16 @@
 import s from './addButton.module.scss'
 import PlusIcon from './assets/plus.svg?react';
+import { useNavigate } from 'react-router';
 export const AddButton = () => {
 
+  const navigate = useNavigate()
+
+  const handleClick = () => {
+    navigate('/range-editor')
+  }
+
   return ( 
-    <button className={s.root}>
+    <button className={s.root} onClick={handleClick}>
       <PlusIcon className={s.icon} />
     </button>
   )
