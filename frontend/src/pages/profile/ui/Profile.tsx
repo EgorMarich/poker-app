8d172dbbd55f56ebea5da234738fe407e6b7f969
e.marichev <e.marichev@msocialproduction.com>
2026-05-23 00:52:48 +0300

    profile

--- frontend/src/pages/profile/ui/Profile.tsx
+++ frontend/src/pages/profile/ui/Profile.tsx
@@ -8,6 +8,8 @@ import { Subscribe } from './subscribe/Subscribe';
 import { PrimaryButton } from '$/shared/ui/buttons/primaryButtons/PrimaryButtons';
 import { useNavigate } from 'react-router';
 import { useTranslation } from 'react-i18next';
+import { typography } from '$/shared/typography/typography';
+import { clsx } from 'clsx';
 
 export const Profile = () => {
   const { data: user } = useProfile();
@@ -23,10 +25,23 @@ export const Profile = () => {
         name={user?.firstName ?? t('profile.title')}
         subscription={subscription}
       />
+      <div className={s.textbook}>
+        <p className={typography({ variant: 'bodySmSemiBold', color: 'white' })}>
+          {t('profile.textbook')}
+        </p>
+        <p className={clsx(s.text, typography({ variant: 'caption', color: 'gray-500' }))}>
+          {t('profile.textbookDesc')}
+        </p>
+        <PrimaryButton onClick={() => navigate('/textbook')}>
+          {t('profile.toTextbook')}
+        </PrimaryButton>
+      </div>
       <TrainingStats stats={stats} />
       <Subscribe />
 
-      { user?.role === 'admin' && ( <PrimaryButton onClick={() => navigate('/admin')}>Перейти в админку</PrimaryButton>)}
+      {user?.role === 'admin' && (
+        <PrimaryButton onClick={() => navigate('/admin')}>Перейти в админку</PrimaryButton>
+      )}
     </div>
   );
 };
