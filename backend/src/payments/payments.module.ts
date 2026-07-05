a8c2a70a1cf650c997e541794c918d1c28707382
e.marichev <e.marichev@msocialproduction.com>
2026-07-05 17:37:43 +0300

    add refferal entity in payments entity

--- backend/src/payments/payments.module.ts
+++ backend/src/payments/payments.module.ts
@@ -5,9 +5,11 @@ import { PaymentsController } from './payments.controller';
 import { Subscription } from './entities/subscription.entity';
 import { Payment } from './entities/payments.entity';
 import { PaymentsService } from './payments.service';
+import { Referral } from '@/refferal/entities/refferal.entity';
+import { ReferralModule } from '@/refferal/refferal.module';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Payment, Subscription])],
+  imports: [TypeOrmModule.forFeature([Payment, Subscription, Referral]), ReferralModule],
   controllers: [PaymentsController],
   providers: [PaymentsService],
   exports: [PaymentsService],
